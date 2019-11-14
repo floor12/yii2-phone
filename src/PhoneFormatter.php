02f3fc7d294f4615d4675cfd0fe36444e420872d
floor12 <floor12@floor12.net>
2019-11-15 00:00:23 +0100

    Formatter refactored and test updated.

--- src/PhoneFormatter.php
+++ src/PhoneFormatter.php
@@ -14,17 +14,28 @@ use yii\helpers\Html;
 class PhoneFormatter
 {
 
-    public static function run($phone)
+    /**
+     * @param $phone
+     * @return string
+     */
+    public static function format($phone)
     {
-        if (preg_match('/^(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})$/', $phone, $matches)) {
-            $result = Html::a("+" . $matches[1] . ' (' . $matches[2] . ') ' . $matches[3] . "-" . $matches[4] . '-' . $matches[5], "tel:+$phone");
-            return $result;
-        }
+        if (preg_match('/^(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})$/', $phone, $matches))
+            return "+" . $matches[1] . ' (' . $matches[2] . ') ' . $matches[3] . "-" . $matches[4] . '-' . $matches[5];
 
-        if (preg_match('/^(\d{2})(\d{3})(\d{3})(\d{2})(\d{2})$/', $phone, $matches)) {
-            $result = Html::a("+" . $matches[1] . ' (' . $matches[2] . ') ' . $matches[3] . "-" . $matches[4] . '-' . $matches[5], "tel:+$phone");
-            return $result;
-        }
+        if (preg_match('/^(\d{2})(\d{3})(\d{3})(\d{2})(\d{2})$/', $phone, $matches))
+            return "+" . $matches[1] . ' (' . $matches[2] . ') ' . $matches[3] . "-" . $matches[4] . '-' . $matches[5];
+
         return $phone;
+    }
+
+    /**
+     * @param $phone
+     * @param array $options
+     * @return string
+     */
+    public static function a($phone, array $options = [])
+    {
+        return Html::a(self::format($phone), "tel:+{$phone}", $options);
     }
 }
--- tests/PhoneFormatterTest.php
+++ tests/PhoneFormatterTest.php
@@ -13,30 +13,53 @@ use floor12\phone\PhoneFormatter;
 
 class PhoneFormatterTest extends \PHPUnit_Framework_TestCase
 {
-    /** Проверяем форматирование невалидного номера, форматирование не пирменяется.
+    /**
+     * If no valid number - just pass it though
      */
     public function testNoValidPhone()
     {
         $phone = 58465;
-        $result = PhoneFormatter::run($phone);
+        $result = PhoneFormatter::format($phone);
         $this->assertEquals($phone, $result);
     }
 
-    /** Проверяем форматирование 11-значного номера номера
+    /**
+     * Format 11 digits number
      */
     public function testPhone11Digits()
     {
         $phone = 79208498755;
-        $result = PhoneFormatter::run($phone);
+        $result = PhoneFormatter::format($phone);
+        $this->assertEquals("+7 (920) 849-87-55", $result);
+    }
+
+    /**
+     * Create link with 11 digits number
+     */
+    public function testPhone11DigitsLink()
+    {
+        $phone = 79208498755;
+        $result = PhoneFormatter::a($phone);
         $this->assertEquals("<a href=\"tel:+{$phone}\">+7 (920) 849-87-55</a>", $result);
     }
 
-    /** Проверяем форматирование 12-значного номера номера
+    /**
+     * Format 12 digits number
      */
     public function testPhone12Digits()
     {
         $phone = 219208498755;
-        $result = PhoneFormatter::run($phone);
+        $result = PhoneFormatter::format($phone);
+        $this->assertEquals("+21 (920) 849-87-55", $result);
+    }
+
+    /**
+     * Create link with 11 digits number
+     */
+    public function testPhone12DigitsLink()
+    {
+        $phone = 219208498755;
+        $result = PhoneFormatter::a($phone);
         $this->assertEquals("<a href=\"tel:+{$phone}\">+21 (920) 849-87-55</a>", $result);
     }
 
