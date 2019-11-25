f983ef97a52f3818424bc008a84eae1b93a0124e
floor12 <floor12@floor12.net>
2019-11-25 20:33:15 +0100

    Fallback method for old api support.

--- src/PhoneFormatter.php
+++ src/PhoneFormatter.php
@@ -18,15 +18,9 @@ class PhoneFormatter
      * @param $phone
      * @return string
      */
-    public static function format($phone)
+    public static function run($phone)
     {
-        if (preg_match('/^(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})$/', $phone, $matches))
-            return "+" . $matches[1] . ' (' . $matches[2] . ') ' . $matches[3] . "-" . $matches[4] . '-' . $matches[5];
-
-        if (preg_match('/^(\d{2})(\d{3})(\d{3})(\d{2})(\d{2})$/', $phone, $matches))
-            return "+" . $matches[1] . ' (' . $matches[2] . ') ' . $matches[3] . "-" . $matches[4] . '-' . $matches[5];
-
-        return $phone;
+        return self::a($phone);
     }
 
     /**
@@ -37,5 +31,20 @@ class PhoneFormatter
     public static function a($phone, array $options = [])
     {
         return Html::a(self::format($phone), "tel:+{$phone}", $options);
+    }
+
+    /**
+     * @param $phone
+     * @return string
+     */
+    public static function format($phone)
+    {
+        if (preg_match('/^(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})$/', $phone, $matches))
+            return "+" . $matches[1] . ' (' . $matches[2] . ') ' . $matches[3] . "-" . $matches[4] . '-' . $matches[5];
+
+        if (preg_match('/^(\d{2})(\d{3})(\d{3})(\d{2})(\d{2})$/', $phone, $matches))
+            return "+" . $matches[1] . ' (' . $matches[2] . ') ' . $matches[3] . "-" . $matches[4] . '-' . $matches[5];
+
+        return $phone;
     }
 }
