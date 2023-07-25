af16bfcd48ba74d57ede166b68f72c570c138da1
Arthur Minasyan <ArtMin96@users.noreply.github.com>
2023-07-25 21:21:13 +0400

    Add Armenian translation

---
+++ src/messages/hy/phone.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'The phone number must contain only numbers.' => 'Հեռախոսահամարը պետք է պարունակի միայն թվեր։',
+    'The phone number must be 11 to 15 digits long.' => 'Հեռախոսահամարը պետք է լինի 11-ից 15 նիշ:'
+];
