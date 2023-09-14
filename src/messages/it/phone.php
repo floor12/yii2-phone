ba5da0c43edd6dcbee10cad5266c363bc067ad20
floor12 <floor12@floor12.net>
2023-09-14 09:46:21 +0200

    Italian lang added.

---
+++ src/messages/it/phone.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'The phone number must contain only numbers.' => 'Il numero di telefono deve contenere solo numeri.',
+    'The phone number must be 11 to 15 digits long.' => 'Il numero di telefono deve essere lungo da 11 a 15 cifre.'
+];
