59c7b71d4780b25b8f982406cb80dc493019195a
floor12 <floor12@floor12.net>
2023-11-07 13:56:31 +0100

    FR lang added.

---
+++ src/messages/fr/phone.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'The phone number must contain only numbers.' => 'Le numéro de téléphone doit contenir uniquement des chiffres.',
+    'The phone number must be 11 to 15 digits long.' => 'Le numéro de téléphone doit comporter de 11 à 15 chiffres.',
+];
